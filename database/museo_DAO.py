14196ad7ac835e1e5c051a860861b6b7cbd2ca5f
andre-saccoo <andre.sacco@icloud.com>
2025-11-04 13:55:38 +0100

    musei

--- database/museo_DAO.py
+++ database/museo_DAO.py
@@ -1,4 +1,5 @@
 from database.DB_connect import ConnessioneDB
+from model.artefattoDTO import Artefatto
 from model.museoDTO import Museo
 
 """
@@ -12,7 +13,7 @@ class MuseoDAO:
 
     @staticmethod
     def  read_museum(): #essendo static method non è necesario il self
-        print ( " lettura da database utilizzando la query" )
+        print ( " lettura musei da database utilizzando la query" )
         risultati = []
         cnx = ConnessioneDB.get_connection()
         if cnx in None:
@@ -27,5 +28,26 @@ class MuseoDAO:
             cursor.close()
             cnx.close()
             return risultati
+
+    @staticmethod
+    def read_artifacts():
+        print ( " lettura artefatti da database utilizzando la query" )
+        risultati = []
+        cnx = ConnessioneDB.get_connection()
+        if cnx in None:
+            print("CONNESSIONE AL DATABASE FALLITA")
+            return None
+        else:
+            cursor = cnx.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM artefatto")
+            for row in cursor:
+                artefatto=Artefatto(row["id"],row["nome"],row["tipologia"], row["epoca"],row["id_museo"])
+                risultati.append(artefatto)
+            cursor.close()
+            cnx.close()
+            return risultati
+
+
+
 
 
